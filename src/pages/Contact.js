d39528704bc6bc171a4c100bd00b080322e83ca4
agan95 <nyonggpj9995@gmail.com>
2022-05-05 03:51:42 +0700

    cearte pagess contact and product

---
+++ src/pages/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div>
+      <h1 className="text-center pt-5">THIS IS CONTACT</h1>
+    </div>
+  );
+}
---
+++ src/pages/Product.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Product() {
+  return (
+    <div>
+      <h1 className="text-center pt-5"> THIS IS PRODUCT</h1>
+    </div>
+  );
+}
