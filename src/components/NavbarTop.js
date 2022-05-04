46ea85c772c51904a6e95b319f1b5bb7438f853a
agan95 <nyonggpj9995@gmail.com>
2022-05-05 03:52:32 +0700

    create navbar

--- src/components/NavbarTop.js
+++ src/components/NavbarTop.js
@@ -2,28 +2,40 @@ import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+import { ModalLogin } from "./ModalLogin";
+
 export default function NavbarTop() {
+  const isLogin = false;
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="md">
       <Container>
-        <Navbar.Brand href="/home">
+        <Navbar.Brand as={Link} to="">
           <img
-            src="https://cdn-icons-png.flaticon.com/512/1051/1051260.png?w=740"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-            alt="React Bootstrap logo"
+            src="https://cdn-icons-png.flaticon.com/512/1216/1216733.png?w=740"
+            width={30}
+            height30
+            alt="icon"
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav>
-            <Nav.Link href="/home">
-              <Link to="/home">Home</Link>
+          <Nav className="justify-content-end flex-grow-1 px-3 text-center">
+            <Nav.Link as={Link} to="">
+              Home
             </Nav.Link>
-            <Nav.Link href="/profile">Profile</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-            <Nav.Link href="/product">Product</Nav.Link>
+            <Nav.Link as={Link} to="product">
+              Product
+            </Nav.Link>
+            <Nav.Link as={Link} to="contact">
+              Contact
+            </Nav.Link>
+            {isLogin ? (
+              <Nav.Link as={Link} to="profile">
+                Profile
+              </Nav.Link>
+            ) : (
+              <ModalLogin />
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
