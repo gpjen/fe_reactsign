91f51b07a0ce8c5aa5c24a6bfbee450ae8faaf28
agan95 <nyonggpj9995@gmail.com>
2022-05-05 13:18:44 +0700

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div className="bgEeeeee">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </div>
 );
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+//import components
+import CardProduct from "../components/CardProduct";
 
 export default function Product() {
+  const [books, seBooks] = useState({});
+
+  // useEffect()
+
   return (
-    <div>
-      <h1 className="text-center pt-5"> THIS IS PRODUCT</h1>
-    </div>
+    <Container>
+      <h1 className="text-center pt-5 mb-5"> THIS IS PRODUCT</h1>
+      <Row>
+        <Col md={3} className="mb-3">
+          <CardProduct />
+        </Col>
+        <Col md={3} className="mb-3">
+          <CardProduct />
+        </Col>
+        <Col md={3} className="mb-3">
+          <CardProduct />
+        </Col>
+        <Col md={3} className="mb-3">
+          <CardProduct />
+        </Col>
+        <Col md={3} className="mb-3">
+          <CardProduct />
+        </Col>
+        <Col md={3} className="mb-3">
+          <CardProduct />
+        </Col>
+      </Row>
+    </Container>
   );
 }
