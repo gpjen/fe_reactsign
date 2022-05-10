509e9e7f818b4a80e20c0ca31b6ec2b8a7465438
agan95 <nyonggpj9995@gmail.com>
2022-05-10 21:31:50 +0700

    fix overflow

--- src/components/CardProduct.js
+++ src/components/CardProduct.js
@@ -1,29 +1,33 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 
 export default function CardProduct() {
   return (
-    <Card className="shadow-lg">
-      <Card.Img
-        variant="top"
-        src="https://cdn.dribbble.com/userupload/2644935/file/original-c17150a1afc270c583b61a134964177a.png?filters:format(webp)?filters%3Aformat%28webp%29=&compress=1&resize=1600x1200"
-      />
+    <Card className="shadow-lg zoomHover">
+      <div className="detailCard">
+        <Card.Img
+          variant="top"
+          src="https://cdn.dribbble.com/userupload/2644935/file/original-c17150a1afc270c583b61a134964177a.png?filters:format(webp)?filters%3Aformat%28webp%29=&compress=1&resize=1600x1200"
+        />
+      </div>
       <Card.Body>
         <Card.Title>book title</Card.Title>
-        <Card.Text>
+        <Card.Text className="fs-5">
           desc is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
+          to additional content...
         </Card.Text>
+        <div className="d-grid gap-2">
+          <Button variant="primary" size="sm">
+            Read
+          </Button>
+          <Button variant="secondary" size="sm">
+            Add
+          </Button>
+        </div>
       </Card.Body>
       <Card.Footer>
         <small className="text-muted">
-          Added By{" "}
-          <img
-            className="rounded"
-            src="https://cdn.dribbble.com/userupload/2669446/file/original-ee4e7360a209eda6a4517eced70b0a0e.jpg?filters:format(webp)?filters%3Aformat%28webp%29=&compress=1&resize=1600x1200"
-            alt="Risman22"
-            height={20}
-          />
+          <p>Added by</p>
         </small>
       </Card.Footer>
     </Card>
