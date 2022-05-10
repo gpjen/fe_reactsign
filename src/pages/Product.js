3b10690be69d5796140517651ee0990c1b6fa952
agan95 <nyonggpj9995@gmail.com>
2022-05-10 21:33:10 +0700

    fix col and row

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,37 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
 //import components
 import CardProduct from "../components/CardProduct";
 
 export default function Product() {
-  const [books, seBooks] = useState({});
+  // const [books, seBooks] = useState({});
 
   // useEffect()
 
   return (
     <Container>
       <h1 className="text-center pt-5 mb-5"> THIS IS PRODUCT</h1>
-      <Row>
-        <Col md={3} className="mb-3">
+      <div className="d-flex flex-wrap">
+        <div>
           <CardProduct />
-        </Col>
-        <Col md={3} className="mb-3">
+        </div>
+        <div>
           <CardProduct />
-        </Col>
-        <Col md={3} className="mb-3">
+        </div>
+        <div>
           <CardProduct />
-        </Col>
-        <Col md={3} className="mb-3">
+        </div>
+        <div>
           <CardProduct />
-        </Col>
-        <Col md={3} className="mb-3">
-          <CardProduct />
-        </Col>
-        <Col md={3} className="mb-3">
-          <CardProduct />
-        </Col>
-      </Row>
+        </div>
+      </div>
     </Container>
   );
 }
